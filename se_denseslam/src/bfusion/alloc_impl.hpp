46e5a904b5c7ab7dccaf404923b65ceea93fc7b9
Unknown <funkn@student.ethz.ch>
2019-07-23 12:07:00 +0100

    Implement voxel traversal algorithm for allocation for OFusion

--- se_denseslam/src/bfusion/alloc_impl.hpp
+++ se_denseslam/src/bfusion/alloc_impl.hpp
@@ -139,4 +139,197 @@ size_t buildOctantList(HashType*              allocationList,
   }
   return (size_t) voxelCount >= reserved ? reserved : (size_t) voxelCount;
 }
+
+template <typename FieldType,
+    template <typename> class OctreeT,
+    typename HashType>
+size_t buildDenseOctantList(HashType*              allocation_list,
+                            size_t                 reserved,
+                            OctreeT<FieldType>&    oct,
+                            const Eigen::Matrix4f& camera_pose,
+                            const Eigen::Matrix4f& K,
+                            const float*           depthmap,
+                            const Eigen::Vector2i& image_size,
+                            const float            voxel_dim,
+                            const float            band,
+                            const int              doubling_ratio) {
+  // Create inverse voxel dimension, camera matrix and projection matrix
+  const float inv_voxel_dim = 1.f/voxel_dim; // inv_voxel_dim := [m] to [voxel]; voxel_dim := [voxel] to [m]
+  Eigen::Matrix4f inv_K = K.inverse();
+  const Eigen::Matrix4f inv_P = camera_pose * inv_K;
+
+  // Read map parameter
+  const int   size = oct.size();
+  const int   max_level = log2(size);
+  const int   leaves_level = max_level - se::math::log2_const(OctreeT<FieldType>::blockSide);
+  const int   side = se::VoxelBlock<FieldType>::side;
+  const int   init_allocation_size = side;
+
+#ifdef _OPENMP
+  std::atomic<unsigned int> voxel_count;
+#else
+  unsigned int voxel_count;
+#endif
+  // Camera position [m] in world frame
+  const Eigen::Vector3f camera_position = camera_pose.topRightCorner<3, 1>();
+  voxel_count = 0;
+#pragma omp parallel for
+  for (int y = 0; y < image_size.y(); ++y) {
+    for (int x = 0; x < image_size.x(); ++x) {
+      if(depthmap[x + y*image_size.x()] == 0)
+        continue;
+      const float depth = depthmap[x + y*image_size.x()];
+      Eigen::Vector3f world_vertex = (inv_P * Eigen::Vector3f((x + 0.5f) * depth,
+                                                              (y + 0.5f) * depth,
+                                                              depth).homogeneous()).head<3>(); //  [m] in world frame
+
+      // Vertex to camera direction in [-] (no unit) in world frame
+      Eigen::Vector3f direction = (camera_position - world_vertex).normalized();
+
+      // Position behind the surface in [m] in world frame
+      const Eigen::Vector3f allocation_origin = world_vertex - (band * 0.5f) * direction;
+
+      // Voxel/node traversal origin to camera distance in [voxel]
+      const float distance = inv_voxel_dim*(camera_position - allocation_origin).norm();
+
+      // Initialise side length in [voxel] of allocated node
+      int allocation_size = init_allocation_size;
+      int allocation_level = max_level - log2(allocation_size);
+
+      Eigen::Vector3f curr_pos_m = allocation_origin;
+      Eigen::Vector3f curr_pos_v = inv_voxel_dim*curr_pos_m;
+      Eigen::Vector3i curr_node = allocation_size*(((curr_pos_v).array().floor())/allocation_size).cast<int>();
+      // Fraction of the current position in [voxel] in the current node along the x-, y- and z-axis
+      Eigen::Vector3f frac = (curr_pos_v - curr_node.cast<float>())/allocation_size;
+
+      //Current state of T in [voxel]
+      Eigen::Vector3f T_max;
+      // Increment/Decrement of voxel value along the ray (-1 or +1)
+      Eigen::Vector3i step_base;
+      // Scaled step_base in [voxel]. Scaling factor will be the current allocation_size
+      Eigen::Vector3i step;
+      // Travelled distance needed in [voxel] to pass a voxel in x, y and z direction
+      Eigen::Vector3f delta_T = allocation_size/direction.array().abs(); // [voxel]/[-]
+
+      // Initalize T
+      if(direction.x() < 0) {
+        step_base.x()  = -1;
+        T_max.x() = frac.x() * delta_T.x();
+      }
+      else {
+        step_base.x() = 1;
+        T_max.x() = (1 - frac.x()) * delta_T.x();
+      }
+      if(direction.y() < 0) {
+        step_base.y()  = -1;
+        T_max.y() = frac.y() * delta_T.y();
+      }
+      else {
+        step_base.y() = 1;
+        T_max.y() = (1 - frac.y()) * delta_T.y();
+      }
+      if(direction.z() < 0) {
+        step_base.z()  = -1;
+        T_max.z() = frac.z() * delta_T.z();
+      }
+      else {
+        step_base.z() = 1;
+        T_max.z() = (1 - frac.z()) * delta_T.z();
+      }
+
+      step = allocation_size*step_base;
+
+      // Distance travelled in [voxel]
+      float travelled = 0;
+
+      do {
+        if ((curr_node.x() < size)
+            && (curr_node.y() < size)
+            && (curr_node.z() < size)
+            && (curr_node.x() >= 0)
+            && (curr_node.y() >= 0)
+            && (curr_node.z() >= 0)) {
+          auto node_ptr = oct.fetch_octant(curr_node.x(), curr_node.y(), curr_node.z(),
+                                           allocation_level);
+          if (!node_ptr) {
+            HashType key = oct.hash(curr_node.x(), curr_node.y(), curr_node.z(),
+                                    std::min(allocation_level, leaves_level));
+            unsigned const idx = voxel_count++;
+            if(voxel_count <= reserved) {
+              allocation_list[idx] = key;
+            }
+          } else if (allocation_level >= leaves_level) {
+            static_cast<se::VoxelBlock<FieldType>*>(node_ptr)->active(true);
+          }
+        }
+
+        // Update allocation variables
+        // Double allocation size every time the allocation distance from the surface is bigger than doubling_ratio * allocation_size
+        if ((travelled - inv_voxel_dim*band/2) > doubling_ratio*allocation_size) {
+          allocation_size = 2*allocation_size;
+
+          // Update current position along the ray where
+          // allocation_origin in [m] and travelled*direction in [voxel]
+          curr_pos_v = inv_voxel_dim*allocation_origin + travelled*direction;
+
+          // Re-initialize the curr_node to match the allocation size
+          curr_node = allocation_size*(((curr_node).array().floor())/allocation_size);
+
+          // Compute fraction of the current position in [voxel] in the updated current node along the x-, y- and z-axis
+          frac = (curr_pos_v - curr_node.cast<float>())/allocation_size;
+
+          // Reduce allocation level to coarser level by 1
+          allocation_level -= 1;
+
+          // Re-initalize delta_T, T_max and step size according to new allocation_size
+          delta_T = allocation_size/direction.array().abs();
+          step = allocation_size*step_base;
+
+          if(direction.x() < 0) {
+            T_max.x() = travelled + frac.x() * delta_T.x();
+          }
+          else {
+            T_max.x() = travelled + (1 - frac.x()) * delta_T.x();
+          }
+          if(direction.y() < 0) {
+            T_max.y() = travelled + frac.y() * delta_T.y();
+          }
+          else {
+            T_max.y() = travelled + (1 - frac.y()) * delta_T.y();
+          }
+          if(direction.z() < 0) {
+            T_max.z() = travelled + frac.z() * delta_T.z();
+          }
+          else {
+            T_max.z() = travelled + (1 - frac.z()) * delta_T.z();
+          }
+        }
+
+        // Traverse to closest face crossing of the voxel block/node (i.e. find minimum T_max)
+        if (T_max.x() < T_max.y()) {
+          if (T_max.x() < T_max.z()) {
+            travelled = T_max.x();
+            curr_node.x() += step.x();
+            T_max.x() += delta_T.x();
+          } else {
+            travelled = T_max.z();
+            curr_node.z() += step.z();
+            T_max.z() += delta_T.z();
+          }
+        } else {
+          if (T_max.y() < T_max.z()) {
+            travelled = T_max.y();
+            curr_node.y() += step.y();
+            T_max.y() += delta_T.y();
+          } else {
+            travelled = T_max.z();
+            curr_node.z() += step.z();
+            T_max.z() += delta_T.z();
+          }
+        }
+      } while (0 < (distance - travelled));
+    }
+  }
+  return (size_t) voxel_count >= reserved ? reserved : (size_t) voxel_count;
+}
 #endif
